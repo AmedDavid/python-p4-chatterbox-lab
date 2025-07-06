cfdf2fcd904906e9427d119f3c2b2840254f721b
David Amedi <amukoahdavid@gmail.com>
2025-07-06 23:50:21 +0300

    Update Pipfile and Pipfile.lock for package version adjustments and add CRUD endpoints for messages in the Flask app

--- server/app.py
+++ server/app.py
@@ -7,7 +7,6 @@ from models import db, Message
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
@@ -16,11 +15,42 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    
+    new_message = Message()
+    new_message.body = data['body']
+    new_message.username = data['username']
+    
+    db.session.add(new_message)
+    db.session.commit()
+    
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    
+    if 'body' in data:
+        message.body = data['body']
+    
+    db.session.commit()
+    
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    
+    db.session.delete(message)
+    db.session.commit()
+    
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
